3e61c60a9681e18a945f493b34543788e2f3e6f9
satoon101 <satoon101@gmail.com>
2026-08-16 13:13:48 -0400

    Updated to disable purchasing buildings for the same criteria as producing them.
    Updated to also disable purchasing when a building has already started production.

--- scripts/UI/ProductionPanel_DistrictBlocker.lua
+++ scripts/UI/ProductionPanel_DistrictBlocker.lua
@@ -33,7 +33,10 @@ function GetData()
             end
         end
     end
+    local progressData = {}
+    local disabledItems = {}
     for _, item in ipairs(data.BuildingItems) do
+        progressData[item.Type] = item.Progress
         if not item.Disabled and item.Progress == 0 then
             local isBlocked, reason = ExposedMembers.CustomDistrictRules.IsBuildingBlocked(
                 playerID,
@@ -45,6 +48,31 @@ function GetData()
             if isBlocked then
                 item.Disabled = true
                 item.ToolTip = item.ToolTip .. "[NEWLINE][COLOR_RED]" .. reason
+                disabledItems[item.Type] = item.ToolTip
+            end
+        end
+    end
+
+    -- the item currently at the front of the queue will not be included,
+    --  so we need to retrieve that value separately
+    local building = GameInfo.Buildings[data.CurrentProductionType]
+    if building ~= nil then
+        local queue = city:GetBuildQueue()
+        local currentProgressAmount = queue:GetBuildingProgress(building.Index)
+        progressData[data.CurrentProductionType] = currentProgressAmount
+    end
+    for _, item in ipairs(data.BuildingPurchases) do
+        if not item.Disabled then
+            local tooltip = disabledItems[item.Type]
+            if tooltip ~= nil then
+                item.Disabled = true
+                item.ToolTip = tooltip
+            else
+                if progressData[item.Type] > 0 then
+                    item.Disabled = true
+                    local reason = "Building process has already begun"
+                    item.ToolTip = item.ToolTip .. "[NEWLINE][COLOR_RED]" .. reason
+                end
             end
         end
     end
